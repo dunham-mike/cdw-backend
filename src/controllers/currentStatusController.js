fa0bc2b71f73bb818b32b2911cde92d44833136e
Mike Dunham <mikedunham@Deekshas-Air.hitronhub.home>
2020-04-28 14:39:41 -0700

    Add logic for current status API.

--- src/controllers/currentStatusController.js
+++ src/controllers/currentStatusController.js
@@ -1,10 +1,19 @@
 const debug = require('debug')('app:currentStatusController');
+const CurrentStatus = require('../models/CurrentStatus');
 
 const currentStatusController = () => {
     const getCurrentStatus = async (req, res) => {
         debug('get request on /current-status');
 
-        res.send('Current Status will be returned here.');
+        CurrentStatus.findOne().sort({createdAt: -1})
+            .then(currentStatus => {
+                debug(currentStatus);
+                res.json(currentStatus);
+            })
+            .catch(error => {
+                debug('[MongooseDB]', error);
+                res.status(404).json({ no_current_status_found: 'Unable to retrieve current status' });
+            });
     }
     
     return { getCurrentStatus };
