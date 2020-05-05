914981b847cec8996f4848ce0b327b5e740358e0
Mike Dunham <mldunham@gmail.com>
2020-05-05 08:27:16 -0700

    Send welcome text when an SMS account is created.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,23 @@
 const debug = require('debug')('app:authController');
-const User = require('../models/User');
 const passport = require('passport');
+const smsService = require('../services/smsService');
 
 const authController = () => {
     const createUserAccount = (req, res) => {
         debug('post request on /api/auth/create-account');
 
-        passport.authenticate('create-account', (err, user) => {            
+        passport.authenticate('create-account', async (err, user) => {            
             if(err) {
                 debug(err);
                 res.send('Error creating new user.');
             } else {
+                if(user.appPreferences.preferredNotificationMethod === "sms") {
+                    const recipientPhoneNumber = user.appPreferences.phoneNumber;
+                    const welcomeMessage = `Welcome to Caltrain Delay Watch! Set up your commute to receive a text any time your usual train is running 10 mins or more late.`;
+                    const messageId = await smsService.sendSMSNotificationToUser(recipientPhoneNumber, welcomeMessage);
+                    debug('Welcome message sent:', messageId);
+                }
+                
                 res.send('Account successfully created.');
             }
         })(req, res);
