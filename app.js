1636895f0ce0d2d35872b471e0622f0523de3af0
Mike Dunham <mldunham@gmail.com>
2020-05-07 11:11:50 -0700

    Update app.js: reorder statements to create logical groupings and remove unnecessary session and cookie-parser packages.

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
 const express = require('express');
 require('dotenv').config();
 const debug = require('debug')('app');
-const connectDB = require('./src/config/db');
 const cors = require('cors');
 const passport = require('passport');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const favicon = require('serve-favicon');
+
+const connectDB = require('./src/config/db');
 require('./src/models/User');
 require('./src/config/passport');
 const authenticateJWT = require('./src/middleware/authenticateJWT');
 const authenticateAdminJWT = require('./src/middleware/authenticateAdminJWT');
 const runScheduledJobs = require('./src/scheduling/scheduledJobs');
-const favicon = require('serve-favicon');
-
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 8082;
+process.env.TZ = 'America/Los_Angeles'; // To make sure alert monitoring runs on the right days of the week, regardless of server time zone
 
 connectDB();
+runScheduledJobs();
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(passport.initialize());
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const adminRouter = require('./src/routes/adminRoutes')();
 const authRouter = require('./src/routes/authRoutes')();
 const timetablesRouter = require('./src/routes/timetablesRoutes')();
 const userDataRouter = require('./src/routes/userDataRoutes')();
 const currentStatusRouter = require('./src/routes/currentStatusRoutes')();
-
-process.env.TZ = 'America/Los_Angeles'; // To make sure alert monitoring runs on the right days of the week, regardless of server time zone
-runScheduledJobs();
-
-app.use(cors({ origin: true, credentials: true }));
-
-app.use(cookieParser());
-
-app.use(passport.initialize());
-
-app.use(favicon(__dirname + '/favicon.ico'));
-
-app.use(morgan('tiny'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/admin', authenticateAdminJWT, adminRouter);
 app.use('/api/auth', authRouter);
